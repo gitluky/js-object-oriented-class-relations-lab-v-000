2267c5c47b3c375853b21aff183e4698f8a96a3a
Jonathan Luk <jluk87@gmail.com>
2019-08-12 01:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ let driverId = 0
 let tripId = 0
 let passenderId = 0
 
-
+const store = [ drivers: [], passengers: [], trips: [] ]
 class Driver {
 
-
+  
 }
 
 class Passenger {
 
-
+  
 }
 
 class Trip {
-
-
+  constructor 
+  
 }
