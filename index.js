141ca648298d588ad3a12f833af5baaab9ce1cc4
Jonathan Luk <jluk87@gmail.com>
2019-08-12 02:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ class Trip {
   driver () {
     return store.drivers.find(
       function (driver) {
-        driver.id === this.driverId;
+        return driver.id === this.driverId;
       }.bind(this)
     );
   }
